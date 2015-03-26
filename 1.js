57bd934105bdd15c3a8f5bad3112d70e0b6f19e3
David Guttman <david@davidguttman.com>
2015-03-26 14:06:00 -0700

    added most basic example

--- 1.js
+++ 1.js
@@ -1,3 +1,53 @@
 module.exports = function() {
-  
+
+  var ss = require('simple-statistics')
+  var accounting = require('accounting')
+  var ReactPivot = require('react-pivot/load')
+
+  var fetch = require('./fetch')
+
+  var url = 'https://docs.google.com:443/spreadsheet/pub'
+    + '?key=0ApW-j0QNGJesdDMwbmhqcFFSVFdfUXR0Vy1XeVdJX1E'
+    + '&single=true&gid=28&output=csv'
+
+  var dimensions = [
+    {title: 'Title', value: 'title'}
+  ]
+
+  var reduce = function(row, memo) {
+    memo = row
+    return memo
+  }
+
+  var calculations = [
+    {title: 'Budget', value: function(row) {return toNumber(row.budget)}, template: fMoney},
+    {title: 'Gross', value: function(row) {return toNumber(row.grossWorldwide)}, value: 'grossWorldwide', template: fMoney}
+  ]
+
+  fetch(url, function(err, rawData) {
+    if (err) return console.error(err)
+    console.log('rawData', rawData)
+
+    ReactPivot(document.body, {
+      rows: rawData,
+      dimensions: dimensions,
+      reduce: reduce,
+      calculations: calculations,
+      activeDimensions: ['Title'],
+      sortBy: 'Title'
+    })
+  })
+
+  function fMoney (val) {
+    return accounting.formatMoney(val)
+  }
+
+  function fNumber (val) {
+    return accounting.formatNumber(val)
+  }
+
+  function toNumber (str) {
+    return parseFloat(str.replace(/,/g, ''))
+  }
+
 }
